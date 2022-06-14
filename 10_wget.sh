b68dbbdef425ab5a2b13ec5c07efd45a45734fe4
Nicolas Cardona <ncardonab@unal.edu.co>
2022-06-13 20:25:23 -0500

    10th class: wget

---
+++ 10_wget.sh
@@ -0,0 +1,7 @@
+# !/bin/bash
+
+
+# Using wget to download resources from the web
+
+echo "Downloading info from internet"
+wget https://www.apache.org/dist/tomcat/tomcat-8/v8.5.78/bin/apache-tomcat-8.5.78.zip 
