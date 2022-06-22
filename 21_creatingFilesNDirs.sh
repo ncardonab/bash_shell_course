d44e9303a5c3645a89bde9aab2b7de118d071fb4
Nicolas Cardona <ncardonab@unal.edu.co>
2022-06-21 19:55:11 -0500

    21st class: Creating dirs and files

---
+++ 21_creatingFilesNDirs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Create a file or directory just passing as arguments [d] for directory and [f] for file and the name"
+
+if [ $1 == "d" ]; then
+	rm -r $2
+	mkdir -m 755 $2
+	echo "Directory created successfully"
+	ls -la | grep $2
+elif [ $1 == "f" ]; then
+	rm $2
+	touch $2
+	echo "File created successfully"
+	ls -la | grep $2
+else
+	echo "This option ($1) does not exists"
+fi
